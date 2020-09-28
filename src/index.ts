969f8f51eb70f1433c10eb363d7c0499a3f4541c
Hyogeun Wang <w@endless9.com>
2020-09-29 05:14:58 +0900

    Pbkdf2Options 추가

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Encryptor } from './Encryptor';
 export { EncryptOptions } from './options/EncryptOptions';
+export { Pbkdf2Options } from './options/Pbkdf2Options';
 export { pbkdf2 } from './pbkdf2';
